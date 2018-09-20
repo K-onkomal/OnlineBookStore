85c3094115de417ca58e4069ff70081707aa5814
K-onkomal <43430843+K-onkomal@users.noreply.github.com>
2018-09-20 15:55:02 +0530

    Update Books.java

--- Books.java
+++ Books.java
@@ -5,8 +5,10 @@ public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
 String bname=sc.next();
 String bauthor=sc.next();
+  String bid=sc.nextInt();
 System.out.println(bname);
 System.out.println(bauthor);
+  System.out.println(bid);
 
 }
 }
