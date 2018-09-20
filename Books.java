6e60b92292c3bb00887c3a9db005e982669b7690
K-onkomal <43430843+K-onkomal@users.noreply.github.com>
2018-09-20 15:53:52 +0530

    Books.java
    This class takes book name  and book author from user

---
+++ Books.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Books{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+String bname=sc.next();
+String bauthor=sc.next();
+System.out.println(bname);
+System.out.println(bauthor);
+
+}
+}
